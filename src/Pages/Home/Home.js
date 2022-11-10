288e0e09fec56aee62f6f3069ca8f2410f36447b
NabilNewaz <nabilnewaz@gmail.com>
2022-11-10 10:59:52 +0600

    Practice Area added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import Services from '../Shared/Services/ServicesHome';
 import Appointment from './Appointment';
 import Headerbanner from './Headerbanner';
+import PracticeArea from './PracticeArea';
 
 const Home = ({ datasize }) => {
     return (
@@ -15,6 +16,13 @@ const Home = ({ datasize }) => {
             </div>
             <div>
                 <Services datasize={datasize}></Services>
+            </div>
+            <div>
+                <div className='text-center px-3 md:px-0'>
+                    <h1 style={{ fontFamily: "Playfair Display, serif" }} className='md:text-4xl text-3xl flex justify-center text-white text-gray-100'>Practice Area</h1>
+                    <p style={{ fontFamily: "Playfair Display, serif" }} className='text-2xl flex justify-center text-gray-400 italic mt-1'>Me always deal with those cases, please stay with us by rating the services.</p>
+                </div>
+                <PracticeArea></PracticeArea>
             </div>
             <div>
                 <div className='text-center px-3 md:px-0'>
---
+++ src/Pages/Home/PracticeArea.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BsFillBriefcaseFill } from "react-icons/bs";
+import { AiFillBank } from "react-icons/ai";
+
+const PracticeArea = () => {
+    return (
+        <div className='container mx-auto md:px-5 px-3 grid lg:grid-cols-4 md:grid-cols-2 gap-4 mt-5 mb-12'>
+            <div className='bg-[url("https://preview.colorlib.com/theme/lawyer/img/practice/1.png")] opacity-80 flex flex-col items-center border-2 rounded-lg bg-no-repeat bg-center p-16'>
+                <BsFillBriefcaseFill className='text-9xl text-white'></BsFillBriefcaseFill>
+                <p className='text-2xl text-white font-bold'>Business Law</p>
+                <p className='text-1xl text-center text-white'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
+            </div>
+            <div className='bg-[url("https://preview.colorlib.com/theme/lawyer/img/practice/2.png")] opacity-80 flex flex-col items-center border-2 rounded-lg bg-no-repeat bg-center p-16'>
+                <AiFillBank className='text-9xl text-white'></AiFillBank>
+                <p className='text-2xl text-white font-bold'>Finance Law</p>
+                <p className='text-1xl text-center text-white'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
+            </div>
+            <div className='bg-[url("https://preview.colorlib.com/theme/lawyer/img/practice/3.png")] opacity-80 flex flex-col items-center border-2 rounded-lg bg-no-repeat bg-center p-16'>
+                <BsFillBriefcaseFill className='text-9xl text-white'></BsFillBriefcaseFill>
+                <p className='text-2xl text-white font-bold'>Family Law</p>
+                <p className='text-1xl text-center text-white'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
+            </div>
+            <div className='bg-[url("https://preview.colorlib.com/theme/lawyer/img/practice/4.png")] opacity-80 flex flex-col items-center border-2 rounded-lg bg-no-repeat bg-center p-16'>
+                <BsFillBriefcaseFill className='text-9xl text-white'></BsFillBriefcaseFill>
+                <p className='text-2xl text-white font-bold'>Education Law</p>
+                <p className='text-1xl text-center text-white'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
+            </div>
+        </div>
+    );
+};
+
+export default PracticeArea;
